6cbd10617b8c584d1349ca8938fa5dec1196952f
Artan-Hasani <artanhasani999@gmail.com>
2024-12-14 15:33:07 +0100

    Added Simple React app

---
+++ portfolio/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Profile from './Components/Profile';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Profile name={'Artan'} />
+      </header>
+    </div>
+  );
+}
+
+export default App;
