f813ad5253314cdf4f6fdba960f97a23d9c9c23d
Artan-Hasani <artanhasani999@gmail.com>
2024-12-21 23:51:10 +0100

    change NavBar component

--- portfolio/src/components/NavBar.jsx
+++ portfolio/src/components/NavBar.jsx
@@ -1,51 +1,115 @@
-import React from 'react';
+// src/components/NavBar.jsx
+import React, { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import { Link as RouterLink } from 'react-router-dom';
-import Link from '@mui/material/Link';
 import Button from '@mui/material/Button';
-import { makeStyles } from '@mui/styles';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
+import PropTypes from 'prop-types';
 
-// Optional: Custom styles using makeStyles
-const useStyles = makeStyles({
-  navLinks: {
-    marginLeft: 'auto',
-    display: 'flex',
-    gap: '1rem',
-  },
-  link: {
-    color: '#fff',
-    textDecoration: 'none',
-  },
-});
+const NavBar = ({ darkMode, toggleDarkMode }) => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
-const NavBar = () => {
-  const classes = useStyles();
+  const handleDrawerToggle = () => {
+    setDrawerOpen(!drawerOpen);
+  };
+
+  const navItems = [
+    { text: 'Home', path: '/' },
+    { text: 'About', path: '/about' },
+  ];
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ width: 250 }}>
+      <List>
+        {navItems.map((item) => (
+          <ListItem button key={item.text} component={RouterLink} to={item.path}>
+            <ListItemText primary={item.text} />
+          </ListItem>
+        ))}
+        {/* Dark Mode Toggle in Drawer */}
+        <ListItem button onClick={toggleDarkMode}>
+          <ListItemText primary={darkMode ? 'Light Mode' : 'Dark Mode'} />
+          <IconButton color="inherit">
+            {darkMode ? <Brightness7Icon /> : <Brightness4Icon />}
+          </IconButton>
+        </ListItem>
+      </List>
+    </Box>
+  );
 
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" component="div">
-            <Link component={RouterLink} to="/" className={classes.link}>
+          {/* Brand Name */}
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <RouterLink to="/" style={{ color: '#fff', textDecoration: 'none' }}>
               My Portfolio
-            </Link>
+            </RouterLink>
           </Typography>
 
-          <Box className={classes.navLinks}>
-            <Button color="inherit" component={RouterLink} to="/">
-              Home
-            </Button>
-            <Button color="inherit" component={RouterLink} to="/about">
-              About
-            </Button>
-          </Box>
+          {/* Desktop Navigation */}
+          {!isMobile && (
+            <Box sx={{ display: 'flex', gap: '1rem', alignItems: 'center' }}>
+              {navItems.map((item) => (
+                <Button
+                  color="inherit"
+                  component={RouterLink}
+                  to={item.path}
+                  key={item.text}
+                >
+                  {item.text}
+                </Button>
+              ))}
+              <IconButton color="inherit" onClick={toggleDarkMode}>
+                {darkMode ? <Brightness7Icon /> : <Brightness4Icon />}
+              </IconButton>
+            </Box>
+          )}
+
+          {/* Mobile Navigation */}
+          {isMobile && (
+            <>
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                edge="end"
+                onClick={handleDrawerToggle}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Drawer
+                anchor="right"
+                open={drawerOpen}
+                onClose={handleDrawerToggle}
+              >
+                {drawer}
+              </Drawer>
+            </>
+          )}
         </Toolbar>
       </AppBar>
     </Box>
   );
+};
+
+NavBar.propTypes = {
+  darkMode: PropTypes.bool.isRequired,
+  toggleDarkMode: PropTypes.func.isRequired,
 };
 
 export default NavBar;
